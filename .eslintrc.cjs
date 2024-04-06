942ef1132cdb437add17c5fcf54c2fdaadcd5ee7
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-04-06 16:03:01 -0400

    Set up test linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 const baseRestrictedSyntax = require('eslint-config-tjw-base/no-restricted-syntax.json');
+const jestRestrictedSyntax = require('eslint-config-tjw-jest/no-restricted-syntax.json');
 
 module.exports = {
   root: true,
@@ -10,12 +11,18 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'tjw-base',
-    'tjw-import'
+    'tjw-import',
+    'tjw-jest'
   ],
   rules: {
+    'capitalize-test-names': 'error',
+    'newline-before-expect-assertion': 'error',
+    'test-names-avoid-should': 'error',
+    'jest/no-deprecated-functions': 'off',
     'no-restricted-syntax': [
       'error',
-      ...baseRestrictedSyntax
+      ...baseRestrictedSyntax,
+      ...jestRestrictedSyntax
     ]
   }
 };
