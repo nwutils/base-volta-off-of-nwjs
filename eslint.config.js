c99298abe73503d40a0b81310f74ec75696f21ee
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2025-05-26 14:42:12 -0400

    Upgrade to ESLint 9

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import pluginJs from '@eslint/js';
+import tjwBase from 'eslint-config-tjw-base';
+import tjwImport from 'eslint-config-tjw-import';
+import tjwJest from 'eslint-config-tjw-jest';
+import pluginImport from 'eslint-plugin-import';
+import pluginJest from 'eslint-plugin-jest';
+
+export default [
+  pluginJs.configs.recommended,
+  pluginImport.flatConfigs.recommended,
+  pluginJest.configs['flat/recommended'],
+  tjwBase.configs.recommended,
+  tjwImport,
+  tjwJest.configs.recommended,
+  {
+    // project specific rules/settings
+    languageOptions: {
+      globals: {
+        vi: true
+      }
+    },
+    rules: {
+      'import/named': 'off',
+      'import/namespace': 'off',
+      'import/no-deprecated': 'off',
+
+      // If this is not turned off, linting throws because it can't find 'jest' install
+      'jest/no-deprecated-functions': 'off',
+      'no-async-promise-executor': 'off'
+    }
+  }
+];
